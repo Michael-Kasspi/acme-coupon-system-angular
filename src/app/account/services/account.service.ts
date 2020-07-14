b93c306d8d0292ff5a005eee767f67e53c3a5a74
Michael-Kasspi <68224982+Michael-Kasspi@users.noreply.github.com>
2020-07-15 01:25:32 +0300

    Added support for upload progress for the profile picture and a delete method

--- src/app/account/services/account.service.ts
+++ src/app/account/services/account.service.ts
@@ -2,6 +2,8 @@ import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {EndpointService} from "../../endpoint/endpoint.service";
 import {Observable} from "rxjs";
+import {map} from 'rxjs/operators';
+import {Account} from '../../model/Account';
 
 @Injectable({
     providedIn: 'root'
@@ -14,10 +16,11 @@ export class AccountService {
     ) {
     }
 
-    public getAccount(): Observable<any> {
-        return this.client.get<any>(
+    public getAccount(): Observable<Account> {
+        return this.client.get<Account>(
             this.endpoint.url + 'accounts/',
-            {withCredentials: true});
+            {withCredentials: true})
+            .pipe(map(account => new Account(account)));
     }
 
     public updateAccount(account, password = ''): Observable<any> {
@@ -48,12 +51,23 @@ export class AccountService {
 
     public uploadProfileImage(image: File = null): Observable<any> {
 
+        if (!image) {
+            throw new Error('Unable to upload image without a file');
+        }
+
         const formData = new FormData();
         formData.append('image', image);
 
         return this.client.post<File>(
             this.endpoint.url + 'accounts/profile/',
             formData,
-            {withCredentials: true});
+            {withCredentials: true, reportProgress: true, observe: 'events'});
+    }
+
+    public deleteProfileImage(): Observable<any> {
+        return this.client.delete<Account>(
+            this.endpoint.url + 'accounts/profile/',
+            {withCredentials: true}
+        );
     }
 }
