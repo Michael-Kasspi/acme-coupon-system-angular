414348792136ac7360c6e17c0e3ad3754220b035
Michael-Kasspi <68224982+Michael-Kasspi@users.noreply.github.com>
2020-07-15 01:20:59 +0300

    Revamped the profile image and changed the account subtitle to email instead of credits
    Added better image selection icon.
    Added upload progress with image preview.
    changed the account subtitle to email instead of credits since credits should be managed from the dashboard.

--- src/app/account/components/account-card/account-card.component.ts
+++ src/app/account/components/account-card/account-card.component.ts
@@ -1,10 +1,13 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {MatDialog} from "@angular/material/dialog";
-import {MatSnackBar} from "@angular/material/snack-bar";
+import {MatDialog} from '@angular/material/dialog';
+import {MatSnackBar} from '@angular/material/snack-bar';
 import {EndpointService} from '../../../endpoint/endpoint.service';
 import {FileUploadDialogComponent} from '../../../dialog/file-upload-dialog/file-upload-dialog.component';
 import {AccountService} from '../../services/account.service';
 import {Account} from '../../../model/Account';
+import {AccountDetailsService} from '../../services/account-details.service';
+import {filter, first, map, tap} from 'rxjs/operators';
+import {HttpEvent, HttpEventType, HttpResponse} from '@angular/common/http';
 
 @Component({
     selector: 'app-account-card',
@@ -15,12 +18,14 @@ export class AccountCardComponent implements OnInit {
 
     @Input()
     account: Account = null;
+    private uploadProgress: number = undefined;
 
     constructor(
         private matDialog: MatDialog,
         private accountService: AccountService,
         private snackBar: MatSnackBar,
-        private endpoint: EndpointService
+        private endpoint: EndpointService,
+        private avatarService: AccountDetailsService
     ) {
     }
 
@@ -28,21 +33,78 @@ export class AccountCardComponent implements OnInit {
     }
 
     selectImage() {
-        this.matDialog.open(FileUploadDialogComponent)
-            .afterClosed()
-            .subscribe(file => {
-                if (file) {
-                    this.accountService
-                        .uploadProfileImage(file)
-                        .subscribe(account => {
-                            this.account.deserialize(account);
-                            this.snackBar.open('Profile image was uploaded successfully')
-                        })
-                }
-            })
+        const matDialogRef = this.getMatDialogRef();
+        const component = matDialogRef.componentInstance;
+        const previewImage$ = this.getPreviewImage$(component);
+        const deleteImage$ = this.getDeleteImage$(component);
+
+        matDialogRef.afterClosed().subscribe(file => {
+            previewImage$?.unsubscribe();
+            deleteImage$?.unsubscribe();
+            if (file) {
+                this.upload(file);
+            }
+        });
     }
 
-    get profilePicture(): string{
+    private getDeleteImage$(component) {
+        return component.deleteFileEvent.pipe(first())
+            .subscribe(_ => {
+                const url = this.account.profilePictureUrl;
+                this.account.profilePictureUrl = null;
+                this.accountService.deleteProfileImage().subscribe(account => {
+                    this.account.deserialize(account);
+                    this.avatarService.account = this.account;
+                    this.snackBar.open('The profile image has been deleted successfully');
+                }, error => this.account.profilePictureUrl = url);
+            });
+    }
+
+    private getPreviewImage$(component) {
+        return component.selectedImagePreviewEvent.pipe(first())
+            .subscribe(preview => {
+                this.account.profilePicturePreview = preview;
+            });
+    }
+
+    private getMatDialogRef() {
+        return this.matDialog.open(FileUploadDialogComponent, {
+            data: {
+                currentImageUrl: this.account.profilePictureUrl,
+                action: 'Upload'
+            }
+        });
+    }
+
+    private upload(file: File) {
+        this.uploadImageWithProgress(file)
+            .subscribe(account => {
+                this.account.deserialize(account);
+                this.avatarService.account = this.account;
+                this.snackBar.open('Profile image was uploaded successfully');
+            });
+    }
+
+    private uploadImageWithProgress(file: File) {
+        return this.accountService
+            .uploadProfileImage(file)
+            .pipe(
+                tap((event: HttpEvent<Account>) => {
+                    if (event.type === HttpEventType.UploadProgress) {
+                        this.uploadProgress = Math.round(100 * event.loaded / event.total);
+                    }
+                }),
+                filter((event: HttpEvent<Account>) => event.type === HttpEventType.Response),
+                tap(_ => this.uploadProgress = undefined),
+                map((event: HttpEvent<Account>) => new Account((event as HttpResponse<Account>).body)),
+            );
+    }
+
+    get profilePicture(): string {
+        const preview = this?.account?.profilePicturePreview;
+        if (preview) {
+            return preview;
+        }
         return (this.endpoint.res + this.account.profilePictureUrl);
     }
 }
