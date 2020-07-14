3886bdc0bc3fc3f3b6f60e3f82e3fa1461ee7696
Michael-Kasspi <68224982+Michael-Kasspi@users.noreply.github.com>
2020-07-14 18:55:45 +0300

    Fixed the Account Meny Fab button shadow on click

--- src/app/account/components/user-menu-button/user-menu-button.module.ts
+++ src/app/account/components/user-menu-button/user-menu-button.module.ts
@@ -1,5 +1,5 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 import {UserMenuButtonComponent} from './user-menu-button.component';
 import {MatButtonModule} from '@angular/material/button';
 import {MatMenuModule} from '@angular/material/menu';
@@ -8,19 +8,18 @@ import {RouterModule} from '@angular/router';
 import {MatDividerModule} from '@angular/material/divider';
 
 
-
 @NgModule({
   declarations: [UserMenuButtonComponent],
   exports: [
     UserMenuButtonComponent
   ],
-  imports: [
-    CommonModule,
-    MatButtonModule,
-    MatMenuModule,
-    MatIconModule,
-    RouterModule,
-    MatDividerModule
-  ]
+    imports: [
+        CommonModule,
+        MatButtonModule,
+        MatMenuModule,
+        MatIconModule,
+        RouterModule,
+        MatDividerModule,
+    ]
 })
 export class UserMenuButtonModule { }
