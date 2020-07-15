277a8aeca7c50b542f775fbc20cc8baccc0d2086
Michael-Kasspi <68224982+Michael-Kasspi@users.noreply.github.com>
2020-07-15 12:59:12 +0300

    Added coupon manager to admin dashboard

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -5,21 +5,25 @@ import {AdminComponent} from './admin.component';
 import {AdminGuard} from './guards/admin.guard';
 
 const routes: Routes = [{
-  path: '', canActivate:[AdminGuard], component: AdminComponent, children: [
-    {path: '', pathMatch: 'full', redirectTo: 'companies'},
-    {
-      path: 'companies',
-      loadChildren: () => import('./company-manager/company-manager.module').then(m => m.CompanyManagerModule)
-    }, {
-      path: 'accounts',
-      loadChildren: () => import('./accounts-manager/accounts-manager.module').then(m => m.AccountsManagerModule)
-    }
-  ]
+    path: '', canActivate: [AdminGuard], component: AdminComponent, children: [
+        {path: '', pathMatch: 'full', redirectTo: 'companies'},
+        {
+            path: 'companies',
+            loadChildren: () => import('./company-manager/company-manager.module').then(m => m.CompanyManagerModule)
+        }, {
+            path: 'accounts',
+            loadChildren: () => import('./accounts-manager/accounts-manager.module').then(m => m.AccountsManagerModule)
+        },
+        {
+            path: 'coupons',
+            loadChildren: () => import('../coupon/coupon-manager/coupon-manager.module').then(m => m.CouponManagerModule)
+        }
+    ]
 },];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
 })
 export class AdminRoutingModule {
 }
