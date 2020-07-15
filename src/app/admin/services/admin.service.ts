31c568bd5187a02903787da1411abd4ac4057206
Michael-Kasspi <68224982+Michael-Kasspi@users.noreply.github.com>
2020-07-15 13:01:23 +0300

    Implemented admin CouponRestService

--- src/app/admin/services/admin.service.ts
+++ src/app/admin/services/admin.service.ts
@@ -1,16 +1,18 @@
 import {Injectable} from '@angular/core';
 import {HttpClient, HttpEvent} from '@angular/common/http';
-import {Observable} from "rxjs";
+import {Observable, throwError} from 'rxjs';
 import {EndpointService} from '../../endpoint/endpoint.service';
 import {UserType} from '../../model/UserType';
 import {CouponRestService} from '../../coupon/services/interfaces/CouponRestService';
 import {Coupon} from '../../model/Coupon';
 import {Category} from '../../model/Category';
+import {map} from 'rxjs/operators';
+import {Company} from '../../model/Company';
 
 @Injectable({
     providedIn: 'root'
 })
-export class AdminService implements CouponRestService{
+export class AdminService implements CouponRestService {
 
     constructor(
         private client: HttpClient,
@@ -34,11 +36,11 @@ export class AdminService implements CouponRestService{
             {withCredentials: true});
     }
 
-    public getAllCompanies(): Observable<any> {
-        return this.client.get<any>(
+    public getAllCompanies(): Observable<Company[]> {
+        return this.client.get<Company[]>(
             `${this.endpoint.url}admin/companies`,
             {withCredentials: true}
-        );
+        ).pipe(map((companies: any[]) => companies.map(company => new Company(company))));
     }
 
     public deleteCompany(id: number): Observable<any> {
@@ -47,37 +49,83 @@ export class AdminService implements CouponRestService{
             {withCredentials: true});
     }
 
-    /*TODO: implement methods*/
-
     addCoupon(coupon: Coupon): Observable<Coupon> {
-        return undefined;
+        if (!coupon) {
+            return throwError('Unable to add coupon without coupon');
+        }
+        return this.client.post<Coupon>(
+            `${this.endpoint.url}admin/coupons/`,
+            coupon.serialize(),
+            {withCredentials: true}
+        ).pipe(map(coupon => new Coupon(coupon)));
     }
 
     deleteCoupon(couponId: number): Observable<any> {
-        return undefined;
+        if (!couponId) {
+            throwError('Unable to delete coupon without id');
+        }
+        return this.client.delete<any>(
+            `${this.endpoint.url}admin/coupons/${couponId}`,
+            {withCredentials: true}
+        );
     }
 
-    deleteImage(couponId: number): Observable<Coupon> {
-        return undefined;
+    deleteCouponImage(couponId: number): Observable<Coupon> {
+        if (!couponId) {
+            throwError('Unable to delete coupon image without id');
+        }
+        return this.client.delete<Coupon>(
+            `${this.endpoint.url}admin/coupons/${couponId}/images`,
+            {withCredentials: true}
+        ).pipe(map(coupon => new Coupon(coupon)));
     }
 
     getAllCategories(): Observable<Category[]> {
-        return undefined;
+        return this.client.get<Category[]>(
+            `${this.endpoint.url}admin/categories`,
+            {withCredentials: true}
+        ).pipe(map((categories: any[]) => categories.map(category => new Category(category))));
     }
 
     getCoupon(couponId: number): Observable<Coupon> {
-        return undefined;
+        if (!couponId) {
+            throwError('Unable to get coupon without id');
+        }
+        return this.client.get<Coupon>(
+            `${this.endpoint.url}admin/coupons/${couponId}`,
+            {withCredentials: true}
+        ).pipe(map(coupon => new Coupon(coupon)));
     }
 
     getCoupons(): Observable<Coupon[]> {
-        return undefined;
+        return this.client.get<Coupon[]>(
+            `${this.endpoint.url}admin/coupons/`,
+            {withCredentials: true}
+        ).pipe(map((coupons: any[]) => coupons.map(coupon => new Coupon(coupon))));
     }
 
     updateCoupon(coupon: Coupon): Observable<Coupon> {
-        return undefined;
+        if (!coupon) {
+            return throwError('Unable to add coupon without coupon');
+        }
+        return this.client.put<Coupon>(
+            `${this.endpoint.url}admin/coupons/`,
+            coupon.serialize(),
+            {withCredentials: true}
+        ).pipe(map(coupon => new Coupon(coupon)));
     }
 
     uploadCouponImage(couponId: number, image: File): Observable<HttpEvent<Coupon>> {
-        return undefined;
+        if (!image) {
+            return throwError('Unable to upload coupon image without image');
+        }
+        const formData = new FormData();
+        formData.append('image', image);
+
+        return this.client.post<Coupon>(
+            `${this.endpoint.url}admin/coupons/${couponId}/images`,
+            formData,
+            {withCredentials: true, reportProgress: true, observe: 'events'}
+        );
     }
 }
