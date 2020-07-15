becde1689155f540b6d73cfab48079ed1b81d556
Michael-Kasspi <68224982+Michael-Kasspi@users.noreply.github.com>
2020-07-15 14:33:33 +0300

    Update all-coupons.module.ts

--- src/app/coupon/coupon-manager/all-coupons/all-coupons.module.ts
+++ src/app/coupon/coupon-manager/all-coupons/all-coupons.module.ts
@@ -7,6 +7,7 @@ import {MatTableModule} from '@angular/material/table';
 import {MatCardModule} from '@angular/material/card';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
+import {MatTooltipModule} from '@angular/material/tooltip';
 
 
 @NgModule({
@@ -17,7 +18,8 @@ import {MatButtonModule} from '@angular/material/button';
         MatTableModule,
         MatCardModule,
         MatIconModule,
-        MatButtonModule
+        MatButtonModule,
+        MatTooltipModule
     ]
 })
 export class AllCouponsModule { }
