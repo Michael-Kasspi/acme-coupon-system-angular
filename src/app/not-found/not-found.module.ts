f6116a696d736368e4ae14b6b6ad01a3b8e27188
Michael-Kasspi <68224982+Michael-Kasspi@users.noreply.github.com>
2020-07-15 04:48:52 +0300

    Revamped the not found message

--- src/app/not-found/not-found.module.ts
+++ src/app/not-found/not-found.module.ts
@@ -1,17 +1,21 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
-import { NotFoundRoutingModule } from './not-found-routing.module';
-import { NotFoundComponent } from './not-found.component';
-import {MatIconModule} from '@angular/material/icon';
+import {NotFoundRoutingModule} from './not-found-routing.module';
+import {NotFoundComponent} from './not-found.component';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatDividerModule} from '@angular/material/divider';
 
 
 @NgModule({
   declarations: [NotFoundComponent],
-  imports: [
-    CommonModule,
-    NotFoundRoutingModule,
-    MatIconModule
-  ]
+    imports: [
+        CommonModule,
+        NotFoundRoutingModule,
+        MatCardModule,
+        MatButtonModule,
+        MatDividerModule
+    ]
 })
 export class NotFoundModule { }
