82ea3e8d6777174ac229fe521c93541575518d08
Michael-Kasspi <68224982+Michael-Kasspi@users.noreply.github.com>
2020-07-15 21:20:03 +0300

    Add Account Feature

--- src/app/admin/accounts-manager/add-account/add-account.module.ts
+++ src/app/admin/accounts-manager/add-account/add-account.module.ts
@@ -3,13 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { AddAccountRoutingModule } from './add-account-routing.module';
 import { AddAccountComponent } from './add-account.component';
+import {AccountManagerFormModule} from '../../../account/components/account-manager-form/account-manager-form.module';
+import {MatCardModule} from '@angular/material/card';
 
 
 @NgModule({
   declarations: [AddAccountComponent],
-  imports: [
-    CommonModule,
-    AddAccountRoutingModule
-  ]
+    imports: [
+        CommonModule,
+        AddAccountRoutingModule,
+        AccountManagerFormModule,
+        MatCardModule
+    ]
 })
 export class AddAccountModule { }
