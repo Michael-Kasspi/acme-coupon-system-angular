b2665d7acc312b33de49f151a2a29f18b48a30ae
Michael-Kasspi <68224982+Michael-Kasspi@users.noreply.github.com>
2020-07-15 04:06:32 +0300

    Added support for users logging in and out and not being logged in

--- src/app/account/services/account-details.service.ts
+++ src/app/account/services/account-details.service.ts
@@ -1,8 +1,10 @@
 import {Injectable} from '@angular/core';
 import {AccountService} from './account.service';
 import {Observable, of, Subject} from 'rxjs';
-import {merge, tap} from 'rxjs/operators';
+import {first, flatMap, merge, tap} from 'rxjs/operators';
 import {Account} from '../../model/Account';
+import {SessionService} from '../../auth/session/session.service';
+import {UserType} from '../../model/UserType';
 
 @Injectable({
     providedIn: 'root'
@@ -12,14 +14,17 @@ export class AccountDetailsService {
     private _account: Account = null;
     private _account$: Subject<Account> = new Subject();
 
-    constructor(private accountService: AccountService) {
+    constructor(
+        private accountService: AccountService,
+        private sessionService: SessionService
+    ) {
+        this.listenForUserChange();
     }
 
     public get account$(): Observable<Account> {
         return new Observable<Account>(subscriber => {
             this.fetchAccount$().subscribe(_ => {
                 subscriber.next(this._account);
-                console.log(this._account);
             }, subscriber.error);
         }).pipe(merge(this._account$));
     }
@@ -28,6 +33,9 @@ export class AccountDetailsService {
         if (account) {
             this._account = account;
             this._account$.next(account);
+        } else {
+            this._account = null;
+            this._account$.next(null);
         }
     }
 
@@ -35,9 +43,29 @@ export class AccountDetailsService {
         if (this._account) {
             return of(this._account);
         }
-        return this.accountService.getAccount().pipe(tap(account => {
-            this.account = account;
+
+        return this.sessionService.userType$().pipe(first(), flatMap(userType => {
+            if (userType === UserType.GUEST) {
+                return of(null);
+            }
+            return this.accountService.getAccount().pipe(first(), tap(account => {
+                this.account = account;
+            }));
         }));
+    }
+
+    private listenForUserChange(): void {
+        this.sessionService.userType$().subscribe(userType => {
+            if (userType === UserType.GUEST) {
+                if (this._account) {
+                    this.account = null;
+                }
+            }
+
+            if (!this._account) {
+                this.fetchAccount$().pipe(first()).subscribe();
+            }
+        });
     }
 
 }
