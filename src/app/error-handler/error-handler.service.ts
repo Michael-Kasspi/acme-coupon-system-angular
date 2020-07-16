b22ca95d4fbcb72005a3abef1f626ae6d7be31ec
Michael-Kasspi <68224982+Michael-Kasspi@users.noreply.github.com>
2020-07-16 20:09:17 +0300

    Navigate user to  login on unauthorized error

--- src/app/error-handler/error-handler.service.ts
+++ src/app/error-handler/error-handler.service.ts
@@ -1,17 +1,19 @@
 import {Injectable} from '@angular/core';
-import {MatSnackBar} from "@angular/material/snack-bar";
-import {HttpErrorResponse} from "@angular/common/http";
-import {throwError} from "rxjs";
-import {MatDialog} from "@angular/material/dialog";
-import {ErrorDetailsDialogComponent} from "./dialog/error-details-dialog/error-details-dialog.component";
-import {ErrorSnackBarComponent} from "./dialog/error-snack-bar/error-snack-bar.component";
+import {MatSnackBar} from '@angular/material/snack-bar';
+import {HttpErrorResponse} from '@angular/common/http';
+import {throwError} from 'rxjs';
+import {MatDialog} from '@angular/material/dialog';
+import {ErrorDetailsDialogComponent} from './dialog/error-details-dialog/error-details-dialog.component';
+import {ErrorSnackBarComponent} from './dialog/error-snack-bar/error-snack-bar.component';
+import {Router} from '@angular/router';
 
 @Injectable({
     providedIn: 'root'
 })
 export class ErrorHandlerService {
+    private static readonly UNAUTHORIZED: number = 401;
 
-    constructor(private snackBar: MatSnackBar, private dialog: MatDialog) {
+    constructor(private snackBar: MatSnackBar, private dialog: MatDialog, private router: Router) {
     }
 
     public handleError(error: HttpErrorResponse) {
@@ -21,12 +23,16 @@ export class ErrorHandlerService {
             {data: {message: error.error.message || error.message || 'No error message is available'}}
         );
 
+        if (error.status === ErrorHandlerService.UNAUTHORIZED) {
+            this.router.navigate(['/login']);
+        }
+
         snackBarRef.onAction().subscribe(
             value => {
                 this.dialog.open(
                     ErrorDetailsDialogComponent,
                     {data: error}
-                )
+                );
             });
 
         return throwError(error);
