1d8bc5d3dbe7d2002cb771a40ec9a66a91fbfdc2
Michael-Kasspi <68224982+Michael-Kasspi@users.noreply.github.com>
2020-12-10 15:39:51 +0200

    category client interface to be implemented by client services

---
+++ src/app/category-manager/category.client.interface.ts
@@ -0,0 +1,16 @@
+import {Category} from '../model/Category';
+import {Observable} from 'rxjs';
+
+export interface CategoryClient {
+
+    getAllCategories(): Observable<Category[]>;
+
+    getCategory(id: number): Observable<Category>;
+
+    addCategory(category: Category): Observable<Category>;
+
+    updateCategory(category: Category): Observable<Category>;
+
+    deleteCategory(id: number): Observable<void>;
+
+}
