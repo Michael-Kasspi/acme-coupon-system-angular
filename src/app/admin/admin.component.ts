eb8395ff8289c4522a5cb7b72026eb8037531752
Michael-Kasspi <68224982+Michael-Kasspi@users.noreply.github.com>
2020-12-10 18:00:26 +0200

    angular cli update

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,7 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {TitleService} from '../title/title.service';
 import {AdminService} from './services/admin.service';
-import {CategoryClient} from '../category-manager/category.client.interface';
 import {CategoryManagerService} from '../category-manager/category-manager.service';
 
 @Component({
@@ -15,7 +14,7 @@ export class AdminComponent implements OnInit {
     constructor(
         private titleService: TitleService,
         private adminService: AdminService,
-        private categoryManagerService: CategoryManagerService
+        private categoryManagerService: CategoryManagerService,
     ) {
         categoryManagerService.client = adminService;
     }
