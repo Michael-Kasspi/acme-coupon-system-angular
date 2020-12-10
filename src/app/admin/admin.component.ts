88278d6fb958505f7df784bcc0726cfb0b041cfc
Michael-Kasspi <68224982+Michael-Kasspi@users.noreply.github.com>
2020-12-10 15:41:38 +0200

    added AdminService as the client of CategoryManagerService

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,14 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {TitleService} from '../title/title.service';
+import {AdminService} from './services/admin.service';
+import {CategoryClient} from '../category-manager/category.client.interface';
+import {CategoryManagerService} from '../category-manager/category-manager.service';
 
 @Component({
-  selector: 'app-admin',
-  templateUrl: './admin.component.html',
-  styleUrls: ['./admin.component.scss']
+    selector: 'app-admin',
+    templateUrl: './admin.component.html',
+    styleUrls: ['./admin.component.scss'],
+    providers: [AdminService, CategoryManagerService]
 })
 export class AdminComponent implements OnInit {
 
-    constructor(private titleService: TitleService) { }
+    constructor(
+        private titleService: TitleService,
+        private adminService: AdminService,
+        private categoryManagerService: CategoryManagerService
+    ) {
+        categoryManagerService.client = adminService;
+    }
 
     ngOnInit(): void {
         this.titleService.append('Admin | Dashboard');
