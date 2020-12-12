0a5a631ee96a1153c5375d5d99dca3669c6c31bd
Michael-Kasspi <68224982+Michael-Kasspi@users.noreply.github.com>
2020-12-12 03:10:00 +0200

    Refactored the category class
    Added description field.
    Added getters and setters.
    Added serialize method.

--- src/app/model/Category.ts
+++ src/app/model/Category.ts
@@ -1,8 +1,10 @@
 import { Deserializable } from './Deserializable';
 
 export class Category implements Deserializable{
-    public id: number;
-    public name: string;
+
+    private _id: number;
+    private _name: string;
+    private _description: string;
 
     constructor(input?: any) {
         if (input != null) {
@@ -10,7 +12,40 @@ export class Category implements Deserializable{
         }
     }
 
+    get serialize(): Object {
+        return {
+            id: this.id,
+            name: this.name,
+            description: this.description
+        }
+    }
+
     deserialize(input: any): this {
        return Object.assign(this, input);
+    }
+
+
+    get id(): number {
+        return this._id;
+    }
+
+    set id(value: number) {
+        this._id = value;
+    }
+
+    get name(): string {
+        return this._name;
+    }
+
+    set name(value: string) {
+        this._name = value;
+    }
+
+    get description(): string {
+        return this._description;
+    }
+
+    set description(value: string) {
+        this._description = value;
     }
 }
